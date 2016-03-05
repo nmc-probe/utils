781a8fff63d1fa1b785fa20b811b6eb707b3ab41
Jeff Bowles <jbowles@newmexicoconsortium.org>
2016-03-05 20:36:06 +0000

    Cleanup

--- rest_api/target_app.py
+++
@@ -1,9 +0,0 @@
-#!flask/bin/python
-
-#api.add_resource(BackstoreList, '/target/api/v1.0/backstores',     endpoint='backstores')
-#api.add_resource(iSCSIList,     '/target/api/v1.0/iscsi_targets',  endpoint='iscsi_targets')
-
-api.add_resource(Target, '/target/api/v1.0/target', endpoint='target')
-
-if __name__ == '__main__':
-    app.run(debug=True)
